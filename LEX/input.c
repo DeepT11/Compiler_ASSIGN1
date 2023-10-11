ea6171f029c643533b522129d61ba3303facc906
Deepshikha11004 <cs21btech11016@iith.ac.in>
2023-10-11 14:20:34 +0530

    first commit

---
+++ LEX/input.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+   float secondinterest=12.00;  
+  
+
+   printf("The rate of interest for the second investment is %f \n", secondinterest);  
+   return 0;  
+} 
---
+++ LEX/output.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  
+int main()  
+{  
+   double  secondinterest=12.00;  
+  
+
+   printf("The rate of interest for the second investment is %f \n", secondinterest);  
+   return 0;  
+} 
